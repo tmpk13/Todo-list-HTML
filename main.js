9bfa1f34fd6547932e8d02f0b3ffa88e4c579552
user <user@localhost.my.domain>
2026-01-07 20:59:09 -0800

    responsive width added

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let listItems = [];
 
 let listContainer = document.getElementById("list-container");
 
+
 items.forEach((listElementText) => {
     // Create new element
     let newListElement = document.createElement("div");
@@ -15,24 +16,30 @@ items.forEach((listElementText) => {
     checkButton.type = "checkbox";
     newListElement.append(checkButton);
     
+
     // Add title
     let title = document.createElement("p");
     // Add text content from list
     title.textContent = listElementText;
     newListElement.append(title);
     
+
     // Add class
     newListElement.classList.add("list-item");
     
+
     // Append this element to the list container
     listContainer.append(newListElement);
     
+
     // Add click events
     newListElement.addEventListener("click", function (e) {
 	if (this.classList.contains("selectable")) {
 	    newListElement.classList.toggle("selected");
 	}
     });
+
+
     // Add double click events
     newListElement.addEventListener("dblclick", function (e) {
 	newListElement.classList.toggle("complete");
@@ -44,6 +51,18 @@ items.forEach((listElementText) => {
     listItems.push(newListElement);
 });
 
+
+
+
+/* --- SELECTION --- */
+
+// Have invis divs between that expand when mouse down/hover
+//document.body.addEventListener("mousedown", (event) => {
+    
+//});
+// document.body.addEventListener("click", (event) => {
+// 	document.body.style.backgroundColor = "red";
+// });
 
 // This needs to be updated to be an event where if any are selected 
 // 	the others are selectable
